0563eccf92ba6a0f71282b6115bc87a1c3f0006f
Rubens Mendes <rubens.mendes@fatec.sp.gov.br>
2022-05-19 21:51:33 -0300

    Encerrado o conteudo sobre promises.

--- aula.js
+++ aula.js
@@ -1,4 +1,49 @@
 
+//3.3.1 Construindo promises
+
+function calculoDemorado(numero) {
+    return new Promise(function (resolve, reject) {
+        let res = 0;
+        for (let i = 1; i <= numero; i++) {
+            res += i;
+        }
+        resolve(res);
+    });
+}
+calculoDemorado(10).then( (resultado) => {
+    console.log(resultado)
+})
+
+function calculoRapidinho (numero){
+    return Promise.resolve((numero * (numero + 1)) / 2);
+}
+calculoRapidinho (10).then(resultado =>{
+    console.log (resultado)
+})
+//Executa primeiro, mesmo que a promise já esteja fullfilled
+console.log('Esperando...')
+
+function calculoRapidinho(numero) {
+    return numero >= 0
+    ? Promise.resolve((numero * (numero + 1)) / 2)
+    : Promise.reject("Somente valores positivos, por favor");
+}
+calculoRapidinho(10)
+.then((resultado) => {
+    console.log(resultado);
+})
+.catch((err) => {
+    console.log(err);
+});
+calculoRapidinho(-1)
+.then((resultado) => {
+    console.log(resultado);
+})
+.catch((err) => {
+    console.log(err);
+});
+console.log("esperando...");
+
 //3.2. O inferno de callbacks
 
 const fs = require("fs");
